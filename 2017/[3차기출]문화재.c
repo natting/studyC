88cf0e9e94f324ca721111f1001c7de37080bbce
natting <39891002+natting@users.noreply.github.com>
2018-06-27 10:43:39 +0900

    Update [3차기출]문화재.c

--- 2017/[3차기출]문화재.c
+++ 2017/[3차기출]문화재.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int N;						//	문화재의 개수
-int area;					//	면적
+int N;			//	문화재의 개수
+int area;		//	면적
 int arr[1000];
 
 int main(void){
 	int i;
 	int pos, height;
-	int max=0, local_max=0;
-	int left_area=0, mid_area=0, right_area=0;
-	int start=1000, end=0;
+	int max = 0, local_max = 0;
+	int left_area = 0, mid_area = 0, right_area = 0;
+	int start = 1000, end = 0;
 	int left_end, right_end;
 
 	scanf("%d", &N);	
 
 	//	코드를 작성하세요
-	for(i=0;i<N;i++){
+	for (i = 0; i < N; i++){
 		scanf("%d %d", &pos, &height);
 		arr[pos] = height;
 		max = max > height ? max : height;
@@ -23,8 +23,8 @@ int main(void){
 		end = end > pos ? end : pos;
 	}
 	
-	local_max=0;
-	for(i=start;i<=end;i++){
+	local_max = 0;
+	for (i = start; i <= end; i++) {
 		local_max = local_max > arr[i] ? local_max : arr[i];
 		if(local_max == max){
 			left_end = i;
@@ -33,10 +33,10 @@ int main(void){
 		left_area += local_max;
 	}
 
-	local_max=0;
-	for(i=end;i>=start;i--){
+	local_max = 0;
+	for(i = end; i >= start; i--) {
 		local_max = local_max > arr[i] ? local_max : arr[i];
-		if(local_max == max){
+		if (local_max == max) {
 			right_end = i;
 			break;
 		}
