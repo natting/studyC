c9d5805e567d1f85945a974b8b002b8070860a3d
natting <39891002+natting@users.noreply.github.com>
2018-07-11 11:12:54 +0900

    Update [연습문제]주유소찾기.c

--- 2017/[연습문제]주유소찾기.c
+++ 2017/[연습문제]주유소찾기.c
@@ -16,13 +16,15 @@ int main() {
 	scanf("%d", &N);
 	
 	for (i = 0; i < N; i++) {
-		scanf("%d", &distance[i]);		
+		scanf("%d", &distance[i]);
+		id[i] = i + 1;
 	}
-	
+#if 0// 입력받을때 id 에 저장을 
 	for (i = 0; i < N; i++) {
 		id[i] = i + 1;
 	}
-	
+#endif
+#if 0//수정 : 오름차순으로
 	for (i = N - 1; i > 0; i--) {
 		for (j = 1; j < i - 1; j++) {
 			if (distance[j] > distance[j+1]) {
@@ -31,6 +33,16 @@ int main() {
 			}
 		}
 	}
+#else
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N - 1; j++) {
+			if (distance[j] > distance[j+1]) {
+				swap(&distance[j], &distance[j+1]);
+				swap(&id[j], &id[j+1]);
+			}
+		}
+	}
+#endif
 
 	for (i = 0; i < N; i++) {
 		printf("%d ", id[i]);
