662f8469762617bc9594ced72e1fe496c795ad51
natting <39891002+natting@users.noreply.github.com>
2019-03-13 16:17:40 +0900

    Create [4차]마리오게임.c

---
+++ 2018/[4차]마리오게임.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int N;//버섯 수
+int P[150000 + 10];//버섯 값
+
+void Input_Data(void){
+	int i;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++){
+		scanf("%d", &P[i]);
+	}
+}
+
+int main(void){
+	int ans = -1;
+	int sum = 0;
+	Input_Data();		//	입력 함수
+	
+	//	코드를 작성하세요
+	sum = P[0];
+	for (int i =1; i < N; i++) {
+		if (P[i] > P[i-1]) {
+			//printf ("- P[%d] : %d, + P[%d] : %d \n", i-1, P[i-1], i, P[i]);
+			sum = sum - P[i-1] + P[i];
+		}
+	}
+	ans = sum;
+	printf("%d\n", ans);		//	정답 출력
+	return 0;
+}
