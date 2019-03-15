81040684cb59d500f51246f9e15e20a0840a21e9
natting <39891002+natting@users.noreply.github.com>
2019-03-15 11:31:38 +0900

    Create DFSQ_template.c

---
+++ 2018/DFSQ_template.c
@@ -0,0 +1,16 @@
+
+
+void main()
+{
+  int rp = 0;// read point
+  int wp = 0;// write point
+  
+  int Q[wp++] = 0;//시작점
+  
+  while (rp < wp) {
+    Start = Q[rp++];//Q get
+    if (....) {
+      Q[wp++] = value;
+    }
+  }
+}
