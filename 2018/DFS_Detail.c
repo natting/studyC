9f43d594b9d5531b7ec046b16c21c019a8281b81
natting <39891002+natting@users.noreply.github.com>
2019-03-20 09:55:10 +0900

    Update DFS_Detail.c

--- 2018/DFS_Detail.c
+++ 2018/DFS_Detail.c
@@ -14,13 +14,33 @@ int cc[] = {0, 0, -1, 1};
 void main()
 {
 	Input_Data();// data 입력 받음 : map 배열과 N(크기) 입력받음
-	
-	int Q[wp++] = 0;//시작점
-  
-  while (rp < wp) {
-    Start = Q[rp++];//Q get
-    if (....) {
-      Q[wp++] = value;
-    }
-  }
+	// visit 배열 큰수로 초기화
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			visit[i][j] = 10000;// 큰수로 초기화
+		}
+	}
+	// queue 시작 준비
+	queue[wp].r = 0; queue[wp].c = 0;//시작점
+	wp++;
+	visit[0][0] = 0;
+	while (rp < wp) {
+		struct Q dist = queue[rp++];// Q에서 하나 끄냄.
+		//상하좌우 검색
+		for (int i = 0; i < 4; i++) {
+			// next r,c(가로,세로이동한 위치)
+			int nr = dist.r + rr[i];
+			int nc = dist.c + cc[i];
+			// 이동하는 위치가 범위 벗어났는지 체크
+			if ((nr < 0) || (nr >= N) || (nc < 0) || (nc >= N)) continue;//go to next
+			// 최소값 구하기 이므로...작은면 갱신..map[][]이 char 이므로 - '0'
+			if (visit[nr][nc] > visit[dist.r][dist.c] + map[nr][nc] - '0') {
+				visit[nr][nc] = visit[dist.r][dist.c] + map[nr][nc] - '0';
+				queue[wp].r = nr; queue[wp].c = nc; // next queueing
+				wp++;// write pointer 증가
+			}
+		}
+	}
+	ans = visit[N-1][N-1];
+	printf ("%d\n", ans);
 }
