bf2c4e8da5bcb13ceaae6350ffeed93dab1fedca
natting <39891002+natting@users.noreply.github.com>
2019-03-22 14:52:52 +0900

    Update [1차]신도시파이프_natting_fail.c

--- 2018/[1차]신도시파이프_natting_fail.c
+++ 2018/[1차]신도시파이프_natting_fail.c
@@ -42,22 +42,28 @@ void Input_Data(void){
 		}
 	}
 }
-
+// down이 연결되어 있나?
+#define DOWN(aaa) 	(aaa==2||aaa==3||aaa==4||aaa==7||aaa==9||aaa==11)
+#define UP(aaa)		(aaa==1||aaa==5||aaa==6||aaa==7||aaa==9||aaa==10||aaa==11)
+#define RIGHT(aaa)	(aaa==1||aaa==3||aaa==6||aaa==7||aaa==8||aaa==10||aaa==11)
+#define LEFT(aaa)	(aaa==1||aaa==4||aaa==5||aaa==8||aaa==10||aaa==11)
 int main(void){
 	int ans = -1;
 	Input_Data();		//	입력 함수
 	
 	//	코드를 작성하세요
 	// 시작점
-	queue[wp].r = X; queue[wp].c = Y;
+	queue[wp].r = Y; queue[wp].c = X;
 	wp++;
-	visit[X][Y] = 1;
-	int next;
+	visit[Y][X] = 1;
+	int next;// next 의 파이프모양
+	int curr;// current 의 파이프모양
 	while (rp < wp) {
 		struct Q dir = queue[rp++];
 		for (int i = 0; i < 4; i ++) {
-			int nr = dir.r + rr[i];
-			int nc = dir.c + cc[i];
+			int nr = dir.r + rr[i];//next Y 좌표
+			int nc = dir.c + cc[i];//next X 좌표
+			curr = map[dir.r][dir.c];
 			// 범위 벗어나는지 체크
 			if ((nr < 0) || (nr >= N) || (nc < 0) || (nc >= N))
 				continue;
@@ -68,22 +74,31 @@ int main(void){
 			// 상i=0/하i=1/좌i=2/우i=3
 			next = map[nr][nc];
 			if (next == 0) continue; //갈곳이 없음.
-			if (i == 0) {
-				//상으로 갈때 Next 가 하가 있는지 2,3,4,7,8,11
-				if (next==2||next==3||next==4||next==7||next==9||next==11)
+			// 현재 상하좌우로 갈수 있는 곳만 체크. next 가 연결되는지 체크
+			if (i == 0 ) { // 상
+				//상으로 갈때 Next 가 하가 있는지 2,3,4,7,8,11 && current 가 상이 있는지
+				if (DOWN(next) && UP(curr)) {
+					printf ("i = 0 [상], next = %d, nr = %d, nc = %d \n", next, nr, nc);
 					visit[nr][nc] = 1;
-			} else if (i == 1) {
-				// 하로 갈때 next 가 상이있는지 2,5,6,7,9,10,11
-				if (next==1||next==5||next==6||next==7||next==9||next==10||next==11)
+				}
+			} else if (i == 1) {//하.
+				// 하로 갈때 next 가 상이있는지 2,5,6,7,9,10,11 && current 가 하가 있는지
+				if (UP(next) && DOWN(curr)) {
+					printf ("i = 1 [하], next = %d, nr = %d, nc = %d \n", next, nr, nc);
 					visit[nr][nc]=1;
-			} else if (i == 2) {
-				// 좌로 갈때 next 가 우가있는지 1,3,6,7,8,10,11
-				if (next==1||next==3||next==6||next==7||next==8||next==10||next==11)
+				}
+			} else if (i == 2) {//좌
+				// 좌로 갈때 next 가 우가있는지 1,3,6,7,8,10,11 && current 가 좌가 있는지
+				if (RIGHT(next) && LEFT(curr)) {
+					printf ("i = 2 [좌], next = %d, nr = %d, nc = %d \n", next, nr, nc);
 					visit[nr][nc] = 1;
-			} else if (i == 3) {
-				// 우로 갈때 next 가 좌가있는지 1,4,5,8,10,11
-				if (next==1||next==4||next==5||next==8||next==10||next==11)
+				}
+			} else if (i == 3) {//우
+				// 우로 갈때 next 가 좌가있는지 1,4,5,8,10,11 && current 가 우가 있는지
+				if (LEFT(next) && RIGHT(curr)) {
+					printf ("i = 3 [우], next = %d, nr = %d, nc = %d \n", next, nr, nc);
 					visit[nr][nc] = 1;
+				}
 			}
 			//큐 갱신
 			queue[wp].r = nr; queue[wp].c = nc;
